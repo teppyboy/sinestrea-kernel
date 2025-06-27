2c328efa0cb387c1e5839ac14974733abf464d62
Nguyễn Thế Hưng <tretrauit@gmail.com>
2025-06-27 17:45:24 +0700

    fix: fix lineage url

--- lineage_diting/init.sh
+++ lineage_diting/init.sh
@@ -2,4 +2,4 @@
 
 cd sinestrea
 rm -rf ./common
-git clone github.com/LineageOS/android_kernel_xiaomi_sm8450 common --depth 1 -b lineage-22.2
+git clone https://github.com/LineageOS/android_kernel_xiaomi_sm8450 common --depth 1 -b lineage-22.2
