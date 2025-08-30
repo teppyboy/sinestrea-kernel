35ebdf8428b818db1efe64c9357bdc2a7cbf8c3e
Nguyễn Thế Hưng <tretrauit@gmail.com>
2025-08-30 18:42:44 +0700

    fix: update prebuilt script to only download clang instead

--- common/5.10/update_prebuilts.sh
+++ common/5.10/update_prebuilts.sh
@@ -1,10 +1,13 @@
 #!/usr/bin/env bash
 
+CLANG_VERSION="clang-r574158"
+
+cd sinestrea/
 echo "Updating prebuilt toolchain to main-kernel..."
-cd sinestrea/prebuilts-master/clang/host/linux-x86
-git fetch aosp main-kernel --depth 1
-git checkout FETCH_HEAD
-cd ../../..
+mkdir -p prebuilts-master/clang/host/linux-x86/$CLANG_VERSION
+curl -L \
+  "https://android.googlesource.com/platform/prebuilts/clang/host/linux-x86/+archive/refs/heads/main-kernel/$CLANG_VERSION.tar.gz" \
+  | tar -xz -C prebuilts-master/clang/host/linux-x86/$CLANG_VERSION
 echo "Editing CLANG_PREBUILT_BIN in build config..."
-sed -i -e 's/clang-r416183b/clang-r574158/g' ./common/build.config.common
+sed -i -e "s/clang-r416183b/$CLANG_VERSION/g" ./common/build.config.common
 echo "Toolchain update complete."
