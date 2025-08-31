c51d182d902eaf72f5e4058892b0e42ea2fae3b0
Nguyễn Thế Hưng <tretrauit@gmail.com>
2025-08-31 10:40:28 +0700

    chore: don't download if exists

--- common/5.10/update_prebuilts.sh
+++ common/5.10/update_prebuilts.sh
@@ -5,10 +5,14 @@ CLANG_BRANCH="main-kernel"
 
 cd sinestrea/
 echo "Updating prebuilt toolchain to $CLANG_BRANCH..."
-mkdir -p prebuilts-master/clang/host/linux-x86/$CLANG_VERSION
-curl -L \
-  "https://android.googlesource.com/platform/prebuilts/clang/host/linux-x86/+archive/refs/heads/$CLANG_BRANCH/$CLANG_VERSION.tar.gz" \
-  | tar -xz -C prebuilts-master/clang/host/linux-x86/$CLANG_VERSION
+if [ ! -d prebuilts-master/clang/host/linux-x86/$CLANG_VERSION ] ; then
+  mkdir -p prebuilts-master/clang/host/linux-x86/$CLANG_VERSION
+  curl -L \
+    "https://android.googlesource.com/platform/prebuilts/clang/host/linux-x86/+archive/refs/heads/$CLANG_BRANCH/$CLANG_VERSION.tar.gz" \
+    | tar -xz -C prebuilts-master/clang/host/linux-x86/$CLANG_VERSION
+else
+  echo "Prebuilt toolchain already exists, skipping download."
+fi
 echo "Editing CLANG_PREBUILT_BIN in build config..."
 sed -i -e "s/clang-r416183b/$CLANG_VERSION/g" ./common/build.config.common
 echo "Toolchain update complete."
